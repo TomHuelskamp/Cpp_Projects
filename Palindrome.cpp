4cfb37e33e1f9f560990d89be965b8b144d49b8d
U-BSD\394544 <394544@SHS-10D9C064E.beaverton.k12.or.us>
2021-10-05 11:17:59 -0700

    adding to git hub

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -1,3 +1,6 @@
+//This project checks if the user's input is a palindrome after removing capitals, punctuation, and spaces.
+//Thomas Huelskamp
+//October 3, 2021
 #include <iostream>
 #include <cstring>
 
@@ -8,40 +11,35 @@ int main()
   char input2[80];
   int a=0;
   bool notPalindrome = false;
+  //Prompts the user to enter characters for character array input
   cin.get(input, 80);
   cin.get();
   
-  for(int i=0; i<80 && input[i]!='\0'; i++){
-    if(input[i]>='A' && input[i]<='Z')//Converts uppercase to lowercase
-      {
+  for(int i=0; i<80 && input[i]!='\0'; i++){//Goes through the input
+    if(input[i]>='A' && input[i]<='Z')
+      {//Replaces capitals with the corresponding lowercase letter
 	input[i]=input[i]+32;
       }
   }
-  for(int i=0; i<80 && input[i]!='\0'; i++){
+  for(int i=0; i<80 && input[i]!='\0'; i++){//Goes through the input (with capitals removed)
     if(input[i] >= 'a' && input[i] <= 'z')
-      {
+      {//if the carachter isn't a space or punctuation, adds the carachter to a new input named input2
 	input2[a]=input[i];
 	++a;
       }
   }
   a--;
-  // cout<<input<<endl;
-  // cout<<input2<<endl;
-  // cout<<"(length)a:"<<a<<endl;
   
-  for(int i=0; i<a; i++){
-    //cout<<i<<":"<<input2[i]<<endl;
-    //cout<<a<<":"<<input2[a]<<endl;
-    if(input2[i]!=input2[a]){
-      //cout<<"tick"<<endl;
+  for(int i=0; i<a; i++){//Goes through input2, using variables i and a for the parrallel characters
+    if(input2[i]!=input2[a]){//Checks if the parrallel characters are the same
       notPalindrome = true;
     }
     a--;
-    }
-  if(notPalindrome){
+  }
+  if(notPalindrome){//If the user's input isn't a palindrome, tell the user
     cout<<"Not a Palindrome"<<endl;
   }
-  else{
+  else{//If the user's input was a palindrome, tell the user
     cout<<"Palindrome."<<endl;
   }
   return 0;
