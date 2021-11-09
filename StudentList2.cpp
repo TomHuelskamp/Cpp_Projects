dc2d9c010ef391d347640cc4a43fc02c179d7bfb
U-BSD\394544 <394544@SHS-10D9C06DF.beaverton.k12.or.us>
2021-11-09 12:23:13 -0800

    adding to git hub

--- StudentList2.cpp
+++ StudentList2.cpp
@@ -3,32 +3,38 @@
 #include <vector>
 using namespace std;
 struct Student{
+  char fname[10];
+  char lname[10];
   int id;
-  int gpa;
+  float gpa;
 };
 
-void function1(vector<Student*> &v);
+void add(vector<Student*> &v);
 int main(){
   vector<Student*> vect;
-  function1(vect);
+  add(vect);
 
+  cout<<vect[0]->fname<<endl;
+  cout<<vect[0]->lname<<endl;
   cout<<vect[0]->id<<endl;
-  cout<<vect[0]->gpa<<endl;
+  cout<<vect[0]->gpa<<endl; 
   
   return 0;
 }
-void function1(vector<Student*> &v){
-  int input;
-  int input2;
-  char name[6] = "hello";
-  cout<<"enter id: ";
-  cin>>input;
-  cout<<"enter gpa: ";
-  cin>>input2;
+void add(vector<Student*> &v){
   
   Student* a = new Student();
-  a->id=input;
-  a->gpa=input2;
+  
+  cout<<"First Name: ";
+  cin.get(a->fname, 10);
+  cin.get();
+  cout<<"Last Name: ";
+  cin.get(a->lname, 10);
+  cin.get();
+  cout<<"Student ID: ";
+  cin>>a->id;
+  cout<<"Grade Point Average: ";
+  cin>>a->gpa;
 
   v.push_back(a);
 }
