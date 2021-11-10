ed43ac8c37fe07c0b07b560bd82167fe7549eb60
U-BSD\394544 <394544@SHS-10D9C06DF.beaverton.k12.or.us>
2021-11-10 13:53:54 -0800

    adding to git hub

--- StudentList.cpp
+++ StudentList.cpp
@@ -1,109 +1,84 @@
-//This project lets the user enter, view, and delete stored information about different students
+//This project creates a database of students based on the user's input and lets them add, remove, and print the students and their information
 //Thomas Huelskamp
-//October 26, 2021
+//November 10,2021
 
 #include <iostream>
 #include <cstring>
 #include <vector>
+#include <cmath>
 
 using namespace std;
-
-
-struct Student {
+//Creates a struct for student
+struct Student{
   char fname[10];
   char lname[10];
   int id;
   float gpa;
 };
-
-void function();
-void Add(vector<Student*> &v);
-void Print(vector<Student*> &v);
-void Delete(vector<Student*> &v);
-
+//declares functions
+void add(vector<Student*> &va);
+void print(vector<Student*> &vp);
+void del(vector<Student*> &vd);
+using namespace std;
 int main(){
-
-  //Student* Bob;
-  //cin>>Bob.fname;
-  //cin>>Bob.lname;
-  //Bob.id=123;
-  //Bob.gpa=123;
-  
+  //creates vector of student pointers
   vector<Student*> vect;
+  char input[10];
+  bool notfalse=true;
 
-  
-  
-  char command[10];
-  
-  char fnameInput[10];
-  char lnameInput[10];
-  int idInput;
-  float gpaInput;
-  
-  cout<<"Enter 'Add', 'Print', or 'Delete' to the student list: ";
-  
-  cin.get(command, 10);
-  cin.ignore(100, '\n');
-  
-  if(command[0]=='A'){//Add
-    Add(vect);
-  }else if(command[0]=='P'){//Print
-    Print(vect);
-  }else if(command[0]=='D'){//Delete
-    Delete(vect);
+  while(notfalse){//while loop never ends unless the user chooses to 'QUIT'
+
+    cout<<"Enter: ";//Takes the user's command
+    cin.get(input,10);
+    cin.get();
+
+
+    if(strcmp(input, "ADD")==0){//ADD
+      add(vect);
+    }else if(strcmp(input, "PRINT")==0){//PRINT
+      print(vect);
+    }else if(strcmp(input, "DELETE")==0){//DELETE
+      del(vect);
+    }else if(strcmp(input, "QUIT")==0){//QUIT
+      break;
+    }
   }
-  cout<<"Enter 'Add', 'Print', or 'Delete' to the student list: ";
-  
-  cin.get(command, 10);
-  cin.ignore(100, '\n');
-  
-  if(command[0]=='A'){//Add
-    Add(vect);
-  }else if(command[0]=='P'){//Print
-    Print(vect);
-  }else if(command[0]=='D'){//Delete
-    Delete(vect);
-  }
-
-  /*cout<<"Enter the first name: ";
-  cin.get(fnameInput, 10);
-  cin.ignore(100, '\n');
-
-  cout<<"Enter the last name: ";
-  cin.get(lnameInput, 10);
-  cin.ignore(100, '\n');
-
-  cout<<"Enter the student ID: ";
-  cin>> idInput;
-
-  cout<<"Enter the GPA: ";
-  cin>> gpaInput;
-  
-  function();
-
-  cout<<fnameInput<<endl;
-  cout<<lnameInput<<endl;
-  */
   return 0;
-
 }
 
-void function(){
-  cout<<"h&ello"<<endl;
-}
+void add(vector<Student*> &va){
+  //Creates a new student and asks the user for all the necessary information
+  Student* a = new Student();
+  cout<<"First Name: ";
+  cin.get(a->fname, 10);
+  cin.get();
+  cout<<"Last Name: ";
+  cin.get(a->lname, 10);
+  cin.get();
+  cout<<"Student ID: ";
+  cin>>a->id;
+  cout<<"Grade Point Average: ";
+  cin>>a->gpa;
+  cin.ignore(100,'\n');
 
-void Add(vector<Student*> &v){
-  cout<<"adding..."<<endl;
-  Student* Bob = new Student();
-  cin>>Bob->fname;
-  cin>>Bob->lname;
-  Bob->id=123;
-  Bob->gpa=123;
-}  
-void Delete(vector<Student*> &v){
-  cout<<"adding..."<<endl;
-  cout<<Bob->fname;
+  va.push_back(a);
 }
-void Print(vector<Student*> &v){
-  cout<<"adding..."<<endl;
+void print(vector<Student*> &vp){
+  for(int i=0; i<vp.size();i++){//goes through the vector of students and tells the user their information
+    float roundedgpa;
+    roundedgpa=round(vp[i]->gpa*100)/100;//rounds the gpa
+    cout<<vp[i]->fname<<" "<<vp[i]->lname<<", "<<vp[i]->id<<", "<<roundedgpa<<endl; 
+  }
+}
+void del(vector<Student*> &vd){
+  //Asks the user for the student's id to delete the student
+  int idinput;
+  cout<<"What ID number would you like to delete?: ";
+  cin>>idinput;
+  cin.ignore(100,'\n');
+  for(int i=0; i<vd.size();i++){//goes through the students looking for the id, and deletes the student with the same id as the user entered
+    if(vd[i]->id==idinput){
+      vd.erase(vd.begin()+i);
+    }
+  }
 }
