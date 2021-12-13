054b0e8f5cd5d1b8b3a3c22bf39059838cfa888c
TomHuelskamp <91155336+TomHuelskamp@users.noreply.github.com>
2021-12-13 11:59:45 -0800

    Add files via upload

---
+++ Zuul/Main.cpp
@@ -0,0 +1,14 @@
+//This is a text-based adventure game
+//Thomas Huelskamp
+//
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include "Room.h"
+
+using namespace std;
+
+int main(){
+  cout<<"Welcome to Airport Adventure! To complete the game collect all the Covid variants in the airport and bring them to the exit. Good luck!"<<endl;
+  return 0;
+}
