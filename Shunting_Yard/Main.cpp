c98abee3db6041be4bf0a09a0e7e966285c30679
TomHuelskamp <91155336+TomHuelskamp@users.noreply.github.com>
2022-02-11 11:42:30 -0800

    fixed empty stack problems

--- Shunting_Yard/Main.cpp
+++ Shunting_Yard/Main.cpp
@@ -9,41 +9,51 @@ void push(node* &,char*);
 void pop(node* &);
 void peek(node*);
 int main(){
-  node* stackh;
-  node* queueh;
-  cout<<"test A"<<endl;
+  node* stackHead = new node();
+  stackHead->value=NULL;
+  node* queueHead;
   bool loop=true;
   while(loop){
-    int choice;
+    char choice[50];
+    cout<<"push, pop, peek, enqueue, or dequeue: ";
+    cin.get(choice, 50);
+    cin.get();
     char *input=new char[1];
-    cin>>choice;
-    if(choice==1){
-      cout<<"testC"<<endl;
+    if(strcmp(choice,"push")==0){
+      cout<<"enter push char: ";
       cin>>input;
-      cout<<"testB"<<endl;
-      push(stackh,input);
-    }else if(choice==2){
-      pop(stackh);
-    }else if(choice==3){
-      peek(stackh);
+      cin.get();
+      push(stackHead,input);
+    }else if(strcmp(choice,"pop")==0){
+      pop(stackHead);
+    }else if(strcmp(choice,"peek")==0){
+      peek(stackHead);
     }
   }
   return 0;
 }
 //need to add null for first new node
 //need to prevent the user from deleting/modifying empty stack
-void push(node* &stackh, char* input){
-  node* stackt = new node();
-  stackt->value = input;
-  stackt->next = stackh;
-  stackh = stackt;
+void push(node* &stackHead, char* input){
+  node* stackTemp = new node();
+  stackTemp->value = input;
+  stackTemp->next = stackHead;
+  stackHead = stackTemp;
 }
-void pop(node* &stackh){
-  node* stackt = new node();
-  stackt=stackh->next;
-  delete stackh;
-  stackh=stackt;
-}
-void peek(node* stackh){
-  cout<<stackh->value<<endl;
+void pop(node* &stackHead){
+  if(stackHead->value==NULL){
+    cout<<"cannot pop, stack is already empty"<<endl;
+  }else{
+    node* stackTemp = new node();
+    stackTemp=stackHead->next;
+    delete stackHead;
+    stackHead=stackTemp;
+  }
+  }
+void peek(node* stackHead){
+  if(stackHead->value==NULL){
+    cout<<"cannot peek, stack is empty"<<endl;
+  }else{
+  cout<<stackHead->value<<endl;
+  }
 }
