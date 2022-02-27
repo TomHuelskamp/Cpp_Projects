f822db376f98fbbd0eca0c8f0e5405ac8eafd190
TomHuelskamp <91155336+TomHuelskamp@users.noreply.github.com>
2022-02-27 12:36:52 -0800

    working on shunting yard

--- Shunting_Yard/Main.cpp
+++ Shunting_Yard/Main.cpp
@@ -2,86 +2,101 @@
 #include <cstring>
 using namespace std;
 struct node{
-  char* value;
+  char value;
   node* next;
 };
-void push(node* &,char*);
+void push(node* &,char);
 void pop(node* &);
-char* peek(node*);
-void enqueue(node* &, char*);
+char peek(node*);
+void enqueue(node* &, char);
 void dequeue(node* &);
-char* test(node*);
+char test(node*);
+void printOutput(node*);
 int main(){
   node* stackHead = new node();
-  stackHead->value=NULL;
+  stackHead->value='\0';
   node* queueHead = new node();
-  queueHead->value=NULL;
+  queueHead->value='\0';
   bool loop=true;
   char expression[50];
   cin.get(expression, 50);
   cin.get();
-  cout<<strlen(expression);
+  int stacklength=0;
+
   for(int i=0; i<strlen(expression);i++){
-    if(expression[i]>='1'&&expression[i]<=9){//number
-      cout<<"number";
-    }else if(expression[i]>='('&&expression[i]<='+'){//operator
-      cout<<"operator";
+    char o2 = expression[i];
+    int o2p;
+    char o2a;
+    if(o2=='+'){
+      o2p=2;
+      o2a='l';
+    }else if(o2=='-'){
+      o2p=2;
+      o2a='l';
+    }else if(o2=='*'){
+      o2p=3;
+      o2a='l';
+    }else if(o2=='/'){
+      o2p=3;
+      o2a='l';
+    }else if(o2=='^'){
+      o2p=4;
+      o2a='r';
     }
-    else if(expression[i]==' '){//space
-      cout<<"space";
+    if(o2>='1'&&o2<='9'){//number
+      enqueue(queueHead, o2);//pushing number to the output queue
+    }
+    
+    else if(o2>='('&&o2<='+'||o2=='-'||o2=='/'||o2=='^'){//operator
+      if((o2=='+'||o2=='-')&& (peek(stackHead)=='*'||peek(stackHead)=='/')){//lower precedence head (multiply or divide)
+	while(stacklength>0){
+	  enqueue(queueHead, peek(stackHead));
+	  pop(stackHead);
+	  stacklength--;
+	}
+	push(stackHead,o2);
+	stacklength++;
+      }else if(o2=='('){//left parentheses
+	push(stackHead, o2);
+	stacklength++;
+      }else if(o2==')'){//right parentheses
+	while(peek(stackHead)!='('){
+	  enqueue(queueHead, peek(stackHead));
+	  pop(stackHead);
+	  stacklength--;
+	}
+	//left parentheses found and discarded:
+	pop(stackHead);
+	stacklength--;
+      }else{//higher precedence head
+	push(stackHead, o2);//pushing operator to the operator stack
+	stacklength++;
+      }
+    }
+    else if(o2==' '){//space
     }
     else{//invalid
-      cout<<"invalid";
+      cout<<endl<<"invalid expression, try again"<<endl;
+      break;
     }
-    cout<<" ";
   }
-  /**while(loop){
-    char choice[50];
-    cout<<"push, pop, peek, enqueue, or dequeue: ";
-    cin.get(choice, 50);
-    cin.get();
-    char *input=new char[1];
-    
-    if(strcmp(choice,"push")==0){
-      cout<<"enter push char: ";
-      cin>>input;
-      cin.get();
-      push(stackHead,input);
-    }
-
-    else if(strcmp(choice,"pop")==0){
-      pop(stackHead);
-    }
-
-    else if(strcmp(choice,"peek")==0){
-      cout<<*(peek(stackHead))<<endl;
-    }
-
-    else if(strcmp(choice,"enqueue")==0){
-      cout<<"enter enqueue char: ";
-      cin>>input;
-      cin.get();
-      enqueue(queueHead, input);
-    }
-    
-    else if(strcmp(choice,"dequeue")==0){
-      dequeue(queueHead);
-    }
-    
-    else if(strcmp(choice,"test")==0){
-      cout<<*(test(queueHead))<<endl;
-      }
-  }**/
+  while(stacklength>0){
+    enqueue(queueHead, peek(stackHead));
+    pop(stackHead);
+    stacklength--;
+  }
+  printOutput(queueHead);
   return 0;
 }
-void push(node* &stackHead, char* input){
+
+void push(node* &stackHead, char input){
   node* stackTemp = new node();
   stackTemp->value = input;
   stackTemp->next = stackHead;
   stackHead = stackTemp;
 }
 void pop(node* &stackHead){
-  if(stackHead->value==NULL){
+  if(stackHead->value=='\0'){
     cout<<"cannot pop, stack is already empty"<<endl;
   }else{
     node* stackTemp = new node();
@@ -89,41 +104,33 @@ void pop(node* &stackHead){
     delete stackHead;
     stackHead=stackTemp;
   }
-  }
-char* peek(node* stackHead){
-  //if(stackHead->value==NULL){
-  //cout<<"cannot peek, stack is empty"<<endl;
-  //return
-      //}else{
-  //cout<<stackHead->value<<endl;
-  //}
+}
+char peek(node* stackHead){
   return stackHead->value;
 }
-void enqueue(node* &queueHead, char* input){
-  //node* queuqueTemp = new node();
-  if(queueHead->value==NULL){//if the queue is empty (there is only a null node)
-    // cout<<"first"<<endl;
+void enqueue(node* &queueHead, char input){
+  if(queueHead->value=='\0'){//if the queue is empty (there is only a null node)
     node* queueTemp = new node();
-    queueTemp->value=NULL;
+    queueTemp->value='\0';
     queueHead->value=input;
     queueHead->next=queueTemp;
   }else{
-    // cout<<"not first"<<endl;
     node* queueTemp = new node();
     queueTemp = queueHead;
-    while(queueTemp->next->next!=NULL){
+    while(queueTemp->next->next!='\0'){
       queueTemp=queueTemp->next;
     }
+    //the new node is right before the tail
     node* queueTemp2 = new node();
     node* queueTail = new node();
-    queueTail->value=NULL;
+    queueTail->value='\0';
     queueTemp2->value=input;
     queueTemp->next=queueTemp2;
     queueTemp2->next=queueTail;
   }
 }
 void dequeue(node* &queueHead){
-  if(queueHead->value==NULL){
+  if(queueHead->value=='\0'){
     cout<<"cannot dequeue, queue is empty"<<endl;
   }else{
     node* queueTemp = new node();
@@ -132,6 +139,12 @@ void dequeue(node* &queueHead){
     queueHead=queueTemp;
   }
 }
-char* test(node* queueHead){
+char test(node* queueHead){
   return queueHead->value;
+}
+void printOutput(node* queueHead){
+  cout<<queueHead->value;
+  if(queueHead->next->value!='\0'){
+    printOutput(queueHead->next);
+  }
 }
