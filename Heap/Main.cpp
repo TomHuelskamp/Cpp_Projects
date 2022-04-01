faa7200b094d2370fa69ac4ac514ddea4bc24a79
TomHuelskamp <91155336+TomHuelskamp@users.noreply.github.com>
2022-04-01 11:05:08 -0700

    add works for user input

--- Heap/Main.cpp
+++ Heap/Main.cpp
@@ -3,6 +3,8 @@
 #include <cmath>
 using namespace std;
 void add(int* &MAXheap,int input,int i);
+void add1(int* &MAXheap,int input);
+void test(int* MAXheap);
 int main(){
   int* MAXheap = new int[100];
   MAXheap[1]=0;//array is 'empty' when the highest value is 0
@@ -11,10 +13,16 @@ int main(){
   }
   bool forever=true;
   while(forever){
-    cout<<"add a number: ";
-    int input;
-    cin>>input;
-    add(MAXheap,input,1);
+    char name[100];
+    if(strcmp(name,"manual")==0){//enter by number
+      cout<<"Enter a number manually: ";
+      int input;
+      cin>>input;
+      add1(MAXheap,input);
+      test(MAXheap);
+    }else{//enter by file
+
+    }
   }
   return 0;
 }
@@ -22,8 +30,11 @@ void add(int* &MAXheap,int input,int i){
   if(MAXheap[i]==0){//index is empty, place int at index
     MAXheap[i]=input;
   }else if(input>MAXheap[i]){//more than index
+    int replace=MAXheap[i];
+    MAXheap[i]=input;
+    add(MAXheap,replace,i);
   }else if(input<MAXheap[i]){//less than index
-    if(input>MAXheap[i*2]){//more than left
+    if((input>MAXheap[i*2]&&MAXheap[i*2+1]!=0 )|| MAXheap[i*2]==0){//more than left
       if(MAXheap[i*2]==0){//left empty
 	MAXheap[i*2]=input;
       }else{//left full
@@ -39,10 +50,30 @@ void add(int* &MAXheap,int input,int i){
 	MAXheap[i*2+1]=input;
 	add(MAXheap,replace,i);
       }
-    }else if(input<MAXheap[i*2]){//less than left
-      add(MAXheap,input,i*2);
+    }else if(input<MAXheap[i*2] && MAXheap[i*2+1]!=0){//less than left
+	add(MAXheap,input,i*2);
     }else if(input<MAXheap[i*2+1]){//less than right
       add(MAXheap,input,i*2+1);
     }
   }
-};
+}
+void test(int* MAXheap){
+  /**for(int i=0; i<50;i++){
+    if(MAXheap[i]!=0){
+      cout<<i<<". "<<MAXheap[i]<<endl;
+    }
+    }
+  cout<<endl;
+  **/
+  
+}
+void add1(int* &MAXheap, int input){
+  for(int i=1; i<100;i++){
+    if(input>MAXheap[i]){
+      int a=MAXheap[i];
+      MAXheap[i]=input;
+      add1(MAXheap,a);
+      break;
+    }
+  }
+}
