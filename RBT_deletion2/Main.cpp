a04718603192dab0bdb2c60eacb4dee9e0adc7ba
TomHuelskamp <91155336+TomHuelskamp@users.noreply.github.com>
2022-05-31 12:23:30 -0700

    Add files via upload

--- RBT_deletion2/Main.cpp
+++ RBT_deletion2/Main.cpp
@@ -1,6 +1,6 @@
-//This program sorts numbers read in from a file or entered in console in a red black tree
+//This program creates a red black tree with addition from file or console, deletion, searching, and quitting
 //Thomas Huelskamp
-//May 19, 2022
+//May 31, 2022
 #include <vector>
 #include <iostream>
 #include <cstring>
@@ -15,26 +15,27 @@ struct node{
   node* right;
   bool lr;//left right
   bool rb;//red black
-  bool nill;
+  bool nill;//null black null node pointers
 };
 void add(node* &root, int num, node* &p, node* &r);//adds numbers into an unbalanced tree
 void print(node* root, int space);//prints tree
 void rotateLeft(node* n, node* &rt);//left rotation
 void rotateRight(node* n, node* &rt);//right rotation
 void insertMaintenance(node* n, node* &rt);//balances the tree after insert
-void remove(node* root, node* &r, int num);
-void search(node* root, int num);
-void deleteMaintenance(node* x, node* &rt);
-void deleteRotateRight(node* &x);
-void deleteRotateLeft(node* x, node* &rt);
+void remove(node* root, node* &r, int num);//removes a number from tree
+void search(node* root, int num);//searches tree for a number
+void deleteMaintenance(node* x, node* &rt);//balances the tree after deletion
+void deleteRotateRight(node* x, node* &rt);//right rotation
+void deleteRotateLeft(node* x, node* &rt);//left rotation
 
-void deleteRotateRight(node* x, node* &rt){
+void deleteRotateRight(node* x, node* &rt){//right rotation for deletion
   node* y=x->left;
   x->left=y->right;
   if(y->right->nill!=true){
     y->right->parent=x;
   }
   y->parent=x->parent;
+  //replaces x with x->left
   if(x->parent==NULL){
     rt=y;
   }else if(x==x->parent->right){
@@ -44,19 +45,20 @@ void deleteRotateRight(node* x, node* &rt){
   }
   y->right=x;
   x->parent=y;
-  //
+  //fixes root
   while(y->parent!=NULL){
     y=y->parent;
   }
   rt=y;
 }
-void deleteRotateLeft(node* x, node* &rt){
+void deleteRotateLeft(node* x, node* &rt){//left rotation for deletion
   node* y=x->right;
   x->right=y->left;
   if(y->left->nill!=true){
     y->left->parent=x;
   }
   y->parent=x->parent;
+  //replaces x with x->right
   if(x->parent==NULL){
     rt=y;
   }else if(x==x->parent->right){
@@ -66,7 +68,7 @@ void deleteRotateLeft(node* x, node* &rt){
   }
   y->left=x;
   x->parent=y;
-  //
+  //fixes root
   while(y->parent!=NULL){
     y=y->parent;
   }
@@ -145,12 +147,12 @@ void rotateLeft(node* n, node* &rt){//used for left rotations
     n->parent=c->parent;
     n->left=c;
     n->left->parent=n;
-    //n->left->right->nill=true;
+
     n->left->right=new node();
     n->left->right->nill=true;
     n->left->right->rb=false;
     n->left->right->parent=n->left;
-    //n->left->right=NULL;
+
     node* nl=n->left;
     while(nl->right->nill!=true){
     nl=nl->right;
@@ -161,7 +163,6 @@ void rotateLeft(node* n, node* &rt){//used for left rotations
     }
     if(rootParentFix){
       rt=n;
-      //rt->parent->nill=true;
       rt->parent=NULL;
     }else{
       n->parent->right=n;
@@ -173,12 +174,12 @@ void rotateLeft(node* n, node* &rt){//used for left rotations
     }
   }else{//ROTATE B, turning an unlinear chain of 3 consecutive numbers into an organized chain from greatest to least
     cout<<"rotate B"<<endl;
-    //n->left->nill=true;
+  
     n->left=new node();
     n->left->parent=n;
     n->left->nill=true;
     n->left->rb=false;
-    //
+    
     l->parent=p;
     l->right=n;
     n->parent=l;
@@ -206,12 +207,12 @@ void rotateRight(node* n, node* &rt){//used for right rotations, this is all ide
     n->parent=c->parent;
     n->right=c;
     n->right->parent=n;
-    //n->right->left->nill=true;
+
     n->right->left=new node();
     n->right->left->nill=true;
     n->right->left->rb=false;
     n->right->left->parent=n->right;
-    //
+    
     node* nr=n->right;
     while(nr->left->nill!=true){
       nr=nr->left;
@@ -233,12 +234,12 @@ void rotateRight(node* n, node* &rt){//used for right rotations, this is all ide
     }
   }else{//ROTATE B
     cout<<"rotate B"<<endl;
-    // n->right->nill=true;
+  
     n->right=new node();
     n->right->parent=n;
     n->right->nill=true;
     n->right->rb=false;
-    //
+   
     r->parent=p;
     r->left=n;
     n->parent=r;
@@ -253,15 +254,12 @@ void add(node* &root, int num, node* &p, node* &rt){//adding numbers into the tr
   if(root==NULL || root->nill==true){//if the node is null, and the program is at the end of a leaf, add the input number
     if(p==root){//if the input is the first number being added, parent is NULL and is black
       root=new node();
-      
-      // root->left=fill;
-      //oot->left->parent=root;
+   
       root->left=new node();
       root->left->nill=true;
       root->left->rb=false;
       root->left->parent=root;
-      //root->right=fill;
-      //root->right->parent=root;
+     
       root->right=new node();
       root->right->nill=true;
       root->right->rb=false;
@@ -286,11 +284,7 @@ void add(node* &root, int num, node* &p, node* &rt){//adding numbers into the tr
       root->right->nill=true;
       root->right->rb=false;
       root->right->parent=root;
-      /** if(p->key>root->key){
-	root->lr=true;
-      }else{
-	root->lr=false;
-	}**/
+
       root->rb=true;
       root->nill=false;
       cout<<"root key: "<<root->key<<endl;
@@ -384,7 +378,6 @@ void remove(node* root, node* &r, int num){
     if(root==NULL||root->nill==true){
       cout<<"number not foud in the tree"<<endl;
     }else{//programiz
-      //node* y=root;
       node* x;
       node* p;
       node* y;
@@ -403,16 +396,13 @@ void remove(node* root, node* &r, int num){
 	}else{
 	  root->parent->right=x;
 	}
-	//node* rootp=root->parent;
 	if(x!=NULL){
-	  x->parent=root->parent;///// PROBLEM
-	  cout<<"parentIF"<<endl;
+	  x->parent=root->parent;
 	}
       }
       //3.
       else if(root->right->nill==true){
 	cout<<"3."<<endl;
-	//p=root->parent
 	x=root->left;
 	if(root->parent==NULL){
 	  r=x;
@@ -422,31 +412,16 @@ void remove(node* root, node* &r, int num){
 	  root->parent->left=x;
 	}
 	x->parent=root->parent;
-	//transplant root, root->left
-	/**if(root->parent==NULL){
-	  r=x;
-	}else if(root==root->parent->left){
-	  root->parent->left=x;
-	}else{
-	  root->parent->right=x;
-	}
-	x->parent=root->parent;
-	**/	
-	//root=x;
-	//root->parent=p;
-	//p->left/right
-	//parent of root
       }
       //4.
       else{
+	cout<<"4."<<endl;
 	node* y=root->left;
 	while(y->right->nill!=true){
 	  y=y->right;
 	}
-	cout<<"y: "<<y->key<<endl;
 	originalrb=y->rb;
 	x=y->left;
-	cout<<"x: "<<x->key<<endl;
 	if(y->parent==NULL){
 	  r=x;
 	}else if(y==y->parent->right){
@@ -456,7 +431,6 @@ void remove(node* root, node* &r, int num){
 	}
 	x->parent=y->parent;
 	
-
 	if(root->parent==NULL){
 	  r=y;
 	}else if(root==root->parent->right){
@@ -469,63 +443,7 @@ void remove(node* root, node* &r, int num){
 	y->left->parent=y;
 	y->right=root->right;
 	y->right->parent=y;
-	y->rb=root->rb;
-	//nill problem?
-	//y->
-	/**
-	cout<<"4."<<endl;
-
-	node* y=root->right;
-	while(y->left->nill!=true){
-	  y=y->left;
-	}
-	//node* y=root->left;
-	//while(y->right->nill!=true){
-	//y=y->right;
-	//}
-	
-	cout<<"y:"<<y->key<<endl;
-	originalrb=y->rb;
-	x=y->right;
-	//x=y->left;
-	//cout<<"x"<<endl;
-	
-	if(y->parent==root){
-	  cout<<"if"<<endl;
-	  x->parent=y;///
-	}else{
-	  cout<<"else"<<endl;
-	  if(y->parent==NULL){
-	    r=x;
-	  }else if(y==y->parent->left){
-	    y->parent->left=x;
-	  }else{
-	    y->parent->right=x;
-	  }
-	  if(x!=NULL){
-	    x->parent=y->parent;
-	  }
-	  
-	  //transplant y, y->right
-	  y->right=root->right;
-	  y->right->parent=y;
-	  // y->left=root->left;
-	  //y->left->parent=y;
-	}
-	//transplant(y, root)
-	node* clone=root;
-	if(root->parent==NULL){
-	  r=y;
-	}else if(root==root->parent->left){
-	  root->parent->left=y;
-	}else{
-	  root->parent->right=y;
-	}	
-	y->parent=root->parent;
-	y->left=clone->left;
-	y->left->parent=y;
-	y->rb=clone->rb;	
-	**/      
+	y->rb=root->rb;      
       }
       delete root;
       
@@ -553,9 +471,6 @@ void search(node* root, int num){
 void deleteMaintenance(node* x, node* &rt){
   print(rt,0);
   cout<<endl<<"Delete Maintenance"<<endl;
-  //if(x==NULL){
-    //cout<<"x is NULL"<<endl;
-    //}
   node* w;
   cout<<x->key<<endl;
   while(x->rb==false&&x->key!=rt->key){
@@ -569,58 +484,41 @@ void deleteMaintenance(node* x, node* &rt){
       if(w->rb==true){
 	//case1
 	cout<<"case 1"<<endl;
-
 	w->rb=false;
 	x->parent->rb=true;
-	cout<<"0004"<<endl;
-	//rotateRight(x->parent,rt);
-	//rotateLeft(x->parent,rt);// original
 	deleteRotateLeft(x->parent, rt);
 	w=x->parent->right;
       }
       if(w->right->rb==false&&w->left->rb==false){
 	//case2
 	cout<<"case 2"<<endl;
-	
 	w->rb=true;
 	x=x->parent;
       }else{
 	if(w->right->rb==false){
 	  //case3
 	  cout<<"case 3"<<endl;
-	  
 	  w->left->rb=false;
 	  w->rb=true;
-	  cout<<"0005"<<endl;
-	  //rotateRight(w,rt);// original
 	  deleteRotateRight(w,rt);
 	  w=x->parent->right;
 	}
 	//case4
 	cout<<"case 4"<<endl;
-
 	w->rb=x->parent->rb;
 	x->parent->rb=false;
 	w->right->rb=false;
-	cout<<"0006"<<endl;
-	//rotateRight(x->parent,rt);
-	//rotateLeft(x->parent, rt);// original;
 	deleteRotateLeft(x->parent,rt);
 	x=rt;
-	//////RT=X
       }
     }
     else{
       cout<<"right"<<endl;
-      //VICE VERSA
       node* w=x->parent->left;
       if(w->rb==true){
 	//case 1
 	w->rb=false;
 	x->parent->rb=true;
-	cout<<"0007"<<endl;
-	//rotateLeft(x->parent,rt);
-	//rotateRight(x->parent,rt);//original
 	deleteRotateRight(x->parent,rt);
 	w=x->parent->left;
       }
@@ -633,8 +531,6 @@ void deleteMaintenance(node* x, node* &rt){
 	//case 3
 	  w->right->rb=false;
 	  w->rb=true;
-	  cout<<"0008"<<endl;
-	  //rotateLeft(w,rt);// original
 	  deleteRotateLeft(w,rt);
 	  w=x->parent->left;
 	}
@@ -642,20 +538,10 @@ void deleteMaintenance(node* x, node* &rt){
 	w->rb=x->parent->rb;
 	x->parent->rb=false;
 	w->left->rb=false;
-	cout<<"0009"<<endl;
-	//rotateLeft(x->parent,rt);
-	//rotateRight(x->parent,rt);// original
 	deleteRotateRight(x->parent,rt);
 	x=rt;
-	//////x=rt
       }
     }
-    //node* c=x;
-    //while(c->parent!=NULL){
-    //c=c->parent;
-    //}
-    
-    //x->rb=false;
   }
   x->rb=false;
 }
