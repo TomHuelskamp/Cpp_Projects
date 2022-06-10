fc11a7cfe8ff012a6af21b3cda0c30982bd90d1c
TomHuelskamp <91155336+TomHuelskamp@users.noreply.github.com>
2022-06-10 11:43:52 -0700

    Add files via upload

--- Graph_Creator/Main.cpp
+++ Graph_Creator/Main.cpp
@@ -81,10 +81,21 @@ void adjacencyTable(node* table[20]){
 struct node{
   char name[10];
 };
-void addVertex(int **connections, node* **names);
+void addVertex(int** connections, node*** names);
+void addEdge(int** connections, node*** names);
 int main(){
-  int connections[20][20];
-  node* names[20][20];
+  int** connections= new int*[20];
+  node*** names=new node**[20];
+  for (int a=0; a<20; a++){
+    connections[a]=new int[20];
+    names[a]=new node*[20];
+  }
+  for (int y=0; y<20; y++){
+    for(int x=0; x<20; x++){
+      connections[y][x]=0;
+      names[y][x]=NULL;
+    }
+  }
   bool loop=true;
   while(loop){
     cout<<"add 'vertex', add 'edge', print 'table', or 'quit'?: ";
@@ -92,9 +103,9 @@ int main(){
     cin.get(input,10);
     cin.get();
     if(strcmp(input,"vertex")==0){
-      
+      addVertex(connections, names);
     }else if(strcmp(input,"edge")==0){
-
+      addEdge(connections, names);
     }else if(strcmp(input,"table")==0){
 
     }else{
@@ -102,5 +113,45 @@ int main(){
   }
   return 0;
 }
-void addVertex(int **connections, node* **names){
+void addVertex(int** connections, node*** names){
+  cout<<"add vertex"<<endl;
+  cout<<"name: ";
+  node* n=new node();
+  cin>>n->name;
+  cin.get();
+  for(int e=0; e<20; e++){
+    if(!names[0][e]){
+      names[0][e]=n;
+      names[e][0]=n;
+      break;
+    }
+  }
+}
+void addEdge(int** connections, node*** names){
+  cout<<"add edge"<<endl;
+  cout<<"name 1: ";
+  char name1[10];
+  cin.get(name1, 10);
+  cin.get();
+  cout<<"name 2: ";
+  char name2[10];
+  cin.get(name2, 10);
+  cin.get();
+  bool found=false;
+  for(int y=0; y<20; y++){
+    for(int x=0; x<20; x++){
+      if((names[y][0]->name==name1 && names[0][x]->name==name2)||(names[y][0]->name==name2 && names[0][x]->name==name1)){
+	int size;
+	cout<<"distance: ";
+	cin>>size;
+	cin.get();
+	connections[y][x]=size;
+	connections[x][y]=size;
+	found=true;
+      }
+    }
+  }
+  if(!found){
+    cout<<"your chosen vertices couldn't be found";
+  }
 }
