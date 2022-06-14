ddc3d172413b86c046ced8566a60836e4d4e21ca
TomHuelskamp <91155336+TomHuelskamp@users.noreply.github.com>
2022-06-14 12:24:33 -0700

    Add files via upload

--- Graph_Creator/Main.cpp
+++ Graph_Creator/Main.cpp
@@ -1,90 +1,14 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
-/**
-struct node{
-  char name[10];
-  node* connections[20];
-  int  weights[20];
-};
-void adjacencyTable(node* table[20]);
-void addVertex(node* table[20]);
-void addEdge(node* table[20]);
-int main(){
-  node* table[20];
-  for(int i=0;i<20;i++){
-    table[i]=NULL;
-  }
-  bool loop=true;
-  while(loop){
-    cout<<"add 'vertex', add 'edge', print adjacency 'table',or 'quit'?: ";
-    char input[10];
-    cin.get(input, 10);
-    cin.get();
-    if(strcmp(input,"vertex")==0){
-      addVertex(table);
-    }else if(strcmp(input,"edge")==0){
-      addEdge(table);
-    }else if(strcmp(input,"table")==0){
-      adjacencyTable(table);
-    }else{
-      break;
-    }
-  }
-  return 0;
-}
-void addVertex(node* table[20]){
-  cout<<"Add Vertex"<<endl;
-  cout<<"name: ";
-  node* n = new node();;
-  cin>>n->name;
-  cin.get();
-  for(int i=0; i<20; i++){
-    if(table[i]==NULL){
-      table[i]=n;
-      cout<<"added"<<endl;
-      break;
-    }else if(strcmp(table[i]->name,n->name)==0){
-      cout<<"a vertex already has this name"<<endl;
-      break;
-    }
-  }
-}
-void addEdge(node* table[20]){
-  cout<<"Add Edge"<<endl;
-  cout<<"first name: ";
-  char n1[20];
-  cout<<"second name: ";
-  char n2[20];
-  cout<<"weight: ";
-  int wt;
-  for(int i=0; i<20; i++){
-    if(table[i]!=NULL){
-      if(strcmp(table[i]->name,n1)==0){
-	for(int j; j<20; j++){
-	}
-      }
-    }else{
-      cout<<"vertices couldn't be found"<<endl;
-    }
-  }
-}
-void adjacencyTable(node* table[20]){
-  for(int i=0; i<20; i++){
-    if(table[i]!=NULL){
-      cout<<table[i]->name<<", ";
-    }
-  }
-  cout<<endl;
-}
-**/
 struct node{
   char name[5];
 };
 void addVertex(int** connections, node*** names);
 void addEdge(int** connections, node*** names);
 void adjacencyTable(int** connections, node*** names);
-///void 
+void removeVertex(int** connections, node*** names);
+void removeEdge(int** connections, node*** names);
 int main(){
   int** connections= new int*[20];
   node*** names=new node**[20];
@@ -112,17 +36,12 @@ int main(){
     }else if(strcmp(input,"print table")==0){
       adjacencyTable(connections,names);
     }else if(strcmp(input,"remove vertex")==0){
-
+      removeVertex(connections,names);
     }else if(strcmp(input,"remove edge")==0){
-
+      removeEdge(connections,names);
     }else if(strcmp(input,"quit")==0){
       break;
     }
-    //else{
-    //cout<<"you entered an invalid input, or a vertex that was more than 5 characters"<<endl;
-    //cin.ignore(200);
-    //}
-    //adjacencyTable(connections,names);
   }
   return 0;
 }
@@ -197,24 +116,41 @@ void adjacencyTable(int** connections, node*** names){
     }
     cout<<endl;
   }
-
-
-  /**for(int y=0; y<20; y++){
-    for(int x=0; x<20; x++){
-      if(names[y][x]!=NULL){
-	cout<<names[y][x]->name;
-      }else{
-	cout<<"_";
-      }
-      cout<<" ";
-    }
-    cout<<endl;
-  }
+}
+void removeVertex(int** connections, node*** names){
+  char vertex[5];
+  cout<<"vertex: ";
+  cin.get(vertex, 5);
+  cin.get();
   for(int y=0; y<20; y++){
     for(int x=0; x<20; x++){
-      cout<<connections[y][x];
-      cout<<" ";
+      if(names[y][x]!=NULL && strcmp(names[y][x]->name,vertex)==0){
+	names[y][x]=NULL;
+	if(y==0){
+	  for(int i=0; i<20; i++){
+	    connections[i][x]=0;
+	  }
+       }else{
+	  for(int i=0; i<20; i++){
+	    connections[y][i]=0;
+	  }
+	}
+      }
     }
-    cout<<endl;
-    }**/
+  }
+}
+void removeEdge(int** connections, node*** names){
+  cout<<"vertex 1: ";//y
+  char vertex1[5];
+  cin.get(vertex1,5);
+  cin.get();
+  cout<<"vertex 2: ";//x
+  char vertex2[5];
+  cin.get(vertex2,5);
+  cin.get();
+  for(int y=0; y<20; y++){
+    for(int x=0; x<20; x++){
+
+    }
+  }
 }
